742bac5f3a68296ced773c746b1c78f8b3f53f67
girlnamedluna <mossmossstudios@gmail.com>
2025-01-30 14:18:03 -0500

    it's annoying here

--- src/main/java/net/saoirse/saoirsefolklore/item/ModItems.java
+++ src/main/java/net/saoirse/saoirsefolklore/item/ModItems.java
@@ -1,10 +1,17 @@
 package net.saoirse.saoirsefolklore.item;
 
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.saoirse.saoirsefolklore.SaoirseFolklore;
+
+import java.util.List;
+import java.util.function.Supplier;
 
 public class ModItems
 {
@@ -14,9 +21,23 @@ public class ModItems
             () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> DEEPIUM_INGOT = ITEMS.register("deepium_ingot",
             () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> DEEPIUM_DUST = ITEMS.register("deepium_dust",
+            () -> new Item(new Item.Properties()));
 
     public static final DeferredItem<Item> EYE_OF_THE_DEEP = ITEMS.register("eye_of_the_deep",
-            () -> new Item(new Item.Properties().stacksTo(16)));
+            () -> new Item(new Item.Properties().stacksTo(16)){
+        @Override
+        public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag){
+            if(Screen.hasShiftDown()) {
+                tooltipComponents.add(Component.translatable("tooltip.saoirsefolklore.eye_of_the_deep.shift_down"));
+            } else {
+                tooltipComponents.add(Component.translatable("tooltip.saoirsefolklore.eye_of_the_deep"));
+            }
+                super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+            }
+    });
+
+
 
 
     public static void register (IEventBus eventBus)
